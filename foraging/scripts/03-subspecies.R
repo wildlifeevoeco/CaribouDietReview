faed2121ade1d31e948954dca7ff01b2b8ae40ce
Quinn Webber <webber.quinn@gmail.com>
2021-06-05 08:44:29 -0230

    Update 03-subspecies.R

--- foraging/scripts/03-subspecies.R
+++ foraging/scripts/03-subspecies.R
@@ -5,7 +5,7 @@ library(ggplot2)
 sub <- fread("output/clean-data.csv")
 
 
-png("figures/fig2.png", width = 6000, height = 4000, units = "px", res = 600)
+png("figures/fig2.png", width = 8000, height = 4000, units = "px", res = 600)
 aa <- ggplot(sub, 
              aes(Subspecies, lichen, fill=Subspecies)) + 
   geom_jitter(aes(color = Subspecies), 
@@ -68,6 +68,38 @@ bb <- ggplot(sub,
         panel.background = element_blank(),
         panel.border = element_rect(colour = "black", fill=NA, size=1)) 
 
-grid.arrange(aa,bb, nrow = 1)
+cc <- ggplot(sub, 
+             aes(Subspecies, vascular, fill=Subspecies)) + 
+  geom_jitter(aes(color = Subspecies), 
+              shape = 16,
+              position = position_jitter(0.2),
+              size = 2,
+              alpha = 0.6) +
+  geom_boxplot(aes(fill = Subspecies), 
+               alpha = 0.5,
+               outlier.color = NA) + 
+  #geom_bar(stat = "identity", na.rm=TRUE, color = "black", alpha = 0.75) +
+  #geom_errorbar(aes(ymin = meanDiet, 
+  #                  ymax = meanDiet + sdDiet), width = 0.2) +
+  ylab("Percent vascular plants in diet") + xlab("") +
+  scale_fill_viridis_d() +
+  scale_color_viridis_d() +
+  ylim(0,100) +
+  ggtitle("C)") +
+  theme(legend.position = 'none',
+        legend.key = element_blank(),
+        legend.text = element_text(size = 12, color = "black"),
+        legend.title = element_blank(),
+        axis.title = element_text(size=18),
+        axis.text.y = element_text(size=12, color = "black"),
+        axis.text.x = element_text(size = 12, color = "black", angle = 45, hjust = 1), 
+        strip.text  = element_text(size = 16),
+        strip.background = element_rect(colour="black", size = 1, fill = "white"),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        panel.border = element_rect(colour = "black", fill=NA, size=1)) 
+
+
+grid.arrange(aa,bb, cc, nrow = 1)
 
 dev.off()
