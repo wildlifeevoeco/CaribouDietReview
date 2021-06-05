30108c7c4db0d5dfec2553d95dcb4f44fb0e3d49
Quinn Webber <webber.quinn@gmail.com>
2021-06-05 08:44:25 -0230

    Update 02-season.R

--- foraging/scripts/02-season.R
+++ foraging/scripts/02-season.R
@@ -7,7 +7,7 @@ library(gridExtra)
 sub <- fread("output/clean-data.csv")
 
 
-png("figures/fig1.png", width = 6000, height = 4000, units = "px", res = 600)
+png("figures/fig1.png", width = 8000, height = 4000, units = "px", res = 600)
 aa <- ggplot(sub, 
        aes(season, lichen, fill=season)) + 
   geom_jitter(aes(color = season), 
@@ -21,7 +21,7 @@ aa <- ggplot(sub,
   #geom_bar(stat = "identity", na.rm=TRUE, color = "black", alpha = 0.75) +
   #geom_errorbar(aes(ymin = meanDiet, 
   #                  ymax = meanDiet + sdDiet), width = 0.2) +
-  ylab("Percent lichen in diet") + xlab("") +
+  ylab("Percent lichens in diet") + xlab("") +
   scale_fill_viridis_d() +
   scale_color_viridis_d() +
   ylim(0,100) +
@@ -52,7 +52,7 @@ bb <- ggplot(sub,
   #geom_bar(stat = "identity", na.rm=TRUE, color = "black", alpha = 0.75) +
   #geom_errorbar(aes(ymin = meanDiet, 
   #                  ymax = meanDiet + sdDiet), width = 0.2) +
-  ylab("Percent graminoid in diet") + xlab("") +
+  ylab("Percent graminoids in diet") + xlab("") +
   scale_fill_viridis_d() +
   scale_color_viridis_d() +
   ylim(0,100) +
@@ -70,7 +70,39 @@ bb <- ggplot(sub,
         panel.background = element_blank(),
         panel.border = element_rect(colour = "black", fill=NA, size=1)) 
 
-grid.arrange(aa,bb, nrow = 1)
+cc <- ggplot(sub, 
+             aes(season, vascular, fill=season)) + 
+  geom_jitter(aes(color = season), 
+              shape = 16,
+              position = position_jitter(0.2),
+              size = 2,
+              alpha = 0.6) +
+  geom_boxplot(aes(fill = season), 
+               alpha = 0.5,
+               outlier.color = NA) + 
+  #geom_bar(stat = "identity", na.rm=TRUE, color = "black", alpha = 0.75) +
+  #geom_errorbar(aes(ymin = meanDiet, 
+  #                  ymax = meanDiet + sdDiet), width = 0.2) +
+  ylab("Percent vascular plants in diet") + xlab("") +
+  scale_fill_viridis_d() +
+  scale_color_viridis_d() +
+  ylim(0,100) +
+  ggtitle("C)") +
+  theme(legend.position = 'none',
+        legend.key = element_blank(),
+        legend.text = element_text(size = 12, color = "black"),
+        legend.title = element_blank(),
+        axis.title = element_text(size=18),
+        axis.text.y = element_text(size=12, color = "black"),
+        axis.text.x = element_text(size = 12, color = "black", angle = 45, hjust = 1), 
+        strip.text  = element_text(size = 16),
+        strip.background = element_rect(colour="black", size = 1, fill = "white"),
+        panel.grid.minor = element_blank(),
+        panel.background = element_blank(),
+        panel.border = element_rect(colour = "black", fill=NA, size=1)) 
+
+
+grid.arrange(aa,bb, cc, nrow = 1)
 
 dev.off()
 
