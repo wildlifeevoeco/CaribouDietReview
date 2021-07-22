5793c4212142ec4455261d4d67ea0cf6586db151
Quinn Webber <webber.quinn@gmail.com>
2021-07-22 16:38:47 -0230

    Update 06-sympatry.R

--- foraging/scripts/06-sympatry.R
+++ foraging/scripts/06-sympatry.R
@@ -19,20 +19,15 @@ sub$sympatric_ungulates <- factor(sub$sympatric_ungulates, levels=c("0", "1",  "
 png("figures/fig3.png", width = 8000, height = 4000, units = "px", res = 600)
 aa <- ggplot(sub, 
              aes(sympatric_ungulates, lichen, fill=sympatric_ungulates)) + 
-  geom_jitter(aes(color = sympatric_ungulates), 
+  geom_jitter(color = "black", 
               shape = 16,
               position = position_jitter(0.2),
               size = 2,
               alpha = 0.6) +
-  geom_boxplot(aes(fill = sympatric_ungulates), 
+  geom_boxplot(fill = "darkgrey", 
                alpha = 0.5,
                outlier.color = NA) + 
-  #geom_bar(stat = "identity", na.rm=TRUE, color = "black", alpha = 0.75) +
-  #geom_errorbar(aes(ymin = meanDiet, 
-  #                  ymax = meanDiet + sdDiet), width = 0.2) +
   ylab("Percent lichen in diet") + xlab("Number of sympatric ungulates") +
-  scale_fill_viridis_d() +
-  scale_color_viridis_d() +
   ylim(0,100) +
   ggtitle("A)") +
   theme(legend.position = 'none',
@@ -50,20 +45,15 @@ aa <- ggplot(sub,
 
 bb <- ggplot(sub, 
              aes(sympatric_ungulates, graminoid, fill=sympatric_ungulates)) + 
-  geom_jitter(aes(color = sympatric_ungulates), 
+  geom_jitter(color = "black", 
               shape = 16,
               position = position_jitter(0.2),
               size = 2,
               alpha = 0.6) +
-  geom_boxplot(aes(fill = sympatric_ungulates), 
+  geom_boxplot(fill = "darkgrey", 
                alpha = 0.5,
                outlier.color = NA) + 
-  #geom_bar(stat = "identity", na.rm=TRUE, color = "black", alpha = 0.75) +
-  #geom_errorbar(aes(ymin = meanDiet, 
-  #                  ymax = meanDiet + sdDiet), width = 0.2) +
   ylab("Percent graminoid in diet") + xlab("Number of sympatric ungulates") +
-  scale_fill_viridis_d() +
-  scale_color_viridis_d() +
   ylim(0,100) +
   ggtitle("B)") +
   theme(legend.position = 'none',
@@ -81,20 +71,15 @@ bb <- ggplot(sub,
 
 cc <- ggplot(sub, 
              aes(sympatric_ungulates, vascular, fill=sympatric_ungulates)) + 
-  geom_jitter(aes(color = sympatric_ungulates), 
+  geom_jitter(color = "black", 
               shape = 16,
               position = position_jitter(0.2),
               size = 2,
               alpha = 0.6) +
-  geom_boxplot(aes(fill = sympatric_ungulates), 
+  geom_boxplot(fill = "darkgrey", 
                alpha = 0.5,
                outlier.color = NA) + 
-  #geom_bar(stat = "identity", na.rm=TRUE, color = "black", alpha = 0.75) +
-  #geom_errorbar(aes(ymin = meanDiet, 
-  #                  ymax = meanDiet + sdDiet), width = 0.2) +
   ylab("Percent vascular plants in diet") + xlab("Number of sympatric ungulates") +
-  scale_fill_viridis_d() +
-  scale_color_viridis_d() +
   ylim(0,100) +
   ggtitle("C)") +
   theme(legend.position = 'none',
