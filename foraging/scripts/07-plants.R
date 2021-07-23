e42982568809efc2d47d2fabb5aa52620ccaf758
Quinn Webber <webber.quinn@gmail.com>
2021-07-23 15:28:15 -0230

    Update 07-plants.R

--- foraging/scripts/07-plants.R
+++ foraging/scripts/07-plants.R
@@ -8,10 +8,14 @@ so <- fread("input/data_source.csv")
 so <- so[, .N, by = c("data_collection", "authors", "year", "included")]
 so[, .N, by = "data_collection"]
 so <- so[included == 1]
+so$author_yr <- as.factor(paste(so$authors, so$year, sep = "_"))
+
 
 ## load plant data
 plants <- fread("input/plants.csv")
 setnames(plants, "study", "authors")
+
+plants$author_yr <- as.factor(paste(plants$authors, plants$year, sep = "_"))
 
 plants <- plants[type_plant != "animal_parts" & 
                  type_plant != "litter"]
@@ -25,9 +29,9 @@ plants$type_plant[plants$type_plant == "sedge"] <- "other"
 sub <- fread("output/clean-data.csv")
 
 ## merge plants database with restricted list of paper used for analyses
-plants <- merge(plants, so[,c("authors", "data_collection", "included", "N")], by = "authors")
+plants <- merge(plants, so[,c("author_yr", "data_collection", "included", "N")], by = "author_yr")
 
-unique(plants$authors)
+unique(plants$author_yr)
 
 ## get rid of NAs
 plants$plant_species[plants$plant_species == ""] <- "no_species"
@@ -43,48 +47,27 @@ length(unique(plants$plant_species)) - 1 ## minus 1 for the "no species" label
 length(unique(plants$genus)) - 1 ## minus 1 for the "no species" label
 
 ## number of unique articles
-length(unique(plants$authors))
+length(unique(plants$author_yr))
 
 ## number of genera per plant type
 plants[plant_species != "no_species"][, uniqueN(genus), by = "type_plant"]
 
-
-sub2 <- sub[, .N, by = c("authors", "Subspecies")][, c("N") := NULL]
-
-### NMDS 
-plants2 <- merge(plants, sub2, by = "authors", allow.cartesian=TRUE)
-
-
-plants2 <- plants2[plant_species != "no_species"]
-plants_long <- plants2[, uniqueN(plant_species), by = c("authors", "Subspecies" ,"plant_species")]
-
-aa <- plants_long[, sum(V1), by = c("plant_species", "authors")]
-
-data_wide <- spread(plants_long, plant_species, V1)
-data_wide[is.na(data_wide)] <- 0
-
-com = data_wide[,2:ncol(data_wide)]
-m_com = as.matrix(com)
-
-nmds = metaMDS(m_com, distance = "bray")
-nmds
-plot(nmds)
-
-
 ## number plant species per article
-common <- plants[plant_species != "no_species"][, .N, by = c("genus")]
+common <- plants[plant_species != "no_species"][, .N, by = c("genus", "type_plant")]
+
+common[, .N, by = "type_plant"]
 
 ## top plants
-length(unique(plants[genus == "Salix"]$authors))
+length(unique(plants[genus == "Salix"]$author_yr))
 length(unique(plants[genus == "Salix"]$species))
-length(unique(plants[genus == "Equisetum"]$authors))
+length(unique(plants[genus == "Equisetum"]$author_yr))
 length(unique(plants[genus == "Equisetum"]$species))
-length(unique(plants[genus == "Carex"]$authors))
-length(unique(plants[genus == "Carex"]$species))
-length(unique(plants[genus == "Dryas"]$authors))
+length(unique(plants[genus == "Cladonia"]$author_yr))
+length(unique(plants[genus == "Cladonia"]$species))
+length(unique(plants[genus == "Dryas"]$author_yr))
 length(unique(plants[genus == "Dryas"]$species))
-length(unique(plants[genus == "Cetraria"]$authors))
-length(unique(plants[genus == "Cetraria"]$species))
+length(unique(plants[genus == "Vaccinium"]$author_yr))
+length(unique(plants[genus == "Vaccinium"]$species))
 
 
 ## breakdown of plants by taxonony
@@ -95,5 +78,9 @@ chisq.test(plant_freq$N)
 
 ## unique number of species per plant type
 plants[, uniqueN(species), by = "type_plant"]
+
+length(unique(plants[type_plant == "moss"]$author_yr))
+length(unique(plants[type_plant == "fungi"]$author_yr))
+length(unique(plants[type_plant == "tree"]$author_yr))
 
 
