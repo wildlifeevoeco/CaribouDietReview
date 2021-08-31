1e30d39772351c3d7626330f6d095612df3c3ff0
Quinn Webber <webber.quinn@gmail.com>
2021-08-31 13:05:47 -0230

    Update 01-prep-data.R, 02-plants.R, and 5 more files...

---
+++ scripts/01-prep-data.R
@@ -0,0 +1,125 @@
+
+
+library(data.table)
+
+## data source
+so <- fread("input/data_source.csv")
+so <- so[, .N, by = c("data_collection", "authors", "year", "included")]
+so[, .N, by = "data_collection"]
+so <- so[included == 1]
+so$author_yr <- as.factor(paste(so$authors, so$year, sep = " "))
+
+
+## Input foraging data
+df <- fread("input/foraging.csv")
+df$author_yr <- as.factor(paste(df$authors, df$year, sep = " "))
+
+
+df <- df[authors != "Denryter et al." & 
+          authors != "White and Trudell" &
+          authors != "McNeill et al." & 
+          authors != "Thompson et al"]
+
+unique(df$author_yr)
+
+df[, unique(journal), by = "author_yr"]
+
+############################## 
+########## SEASON ############
+############################## 
+
+df <- df[!is.na(season)]
+
+## combine vascular plants
+df$shrubs[is.na(df$shrubs)] <- 0
+df$forbs[is.na(df$forbs)] <- 0
+df$tree[is.na(df$tree)] <- 0
+df$vascular <-  df$shrubs + df$forbs + df$tree
+
+
+df[, mean(forbs, na.rm = T), by = "season"]
+
+## by season
+df$season[df$season == "calving"] <- "1-Calving"
+df$season[df$season == "summer"] <- "2-Summer"
+df$season[df$season == "fall"] <- "3-Autumn"
+df$season[df$season == "winter"] <- "4-Winter"
+df$season[df$season == "spring"] <- "5-Spring"
+
+unique(df$Subspecies)
+
+df$Subspecies[df$Subspecies == "Peary"] <- "1-Peary"
+df$Subspecies[df$Subspecies == "Greenland"] <- "2-Barren Ground"
+df$Subspecies[df$Subspecies == "barren-ground"] <- "2-Barren Ground"
+df$Subspecies[df$Subspecies == "mountaine"] <- "3-Mountain"
+df$Subspecies[df$Subspecies == "woodland"] <- "4-Woodland"
+df$Subspecies[df$Subspecies == "reindeer"] <- "5-Reindeer"
+df$Subspecies[df$Subspecies == "introduced-reindeer"] <- "6-Introduced-Reindeer"
+
+df <- df[,c("lichen", "graminoid", "shrubs", "forbs", "horsetail", "tree", "moss", "fungi", "vascular",
+            "data type", "latitude", "longitude",
+            "sympatric_ungulates", "aug_daily_average",
+            "Subspecies", "season","authors", "author_yr")]
+
+### export data 
+fwrite(df, "output/clean-data.csv")
+
+
+## summary of plant type by season
+data.table(rbind(df[,mean(lichen, na.rm = T), by = "season"], 
+                 df[,mean(graminoid, na.rm = T), by = "season"], 
+                 df[,mean(forbs, na.rm = T), by = "season"], 
+                 df[,mean(shrubs, na.rm = T), by = "season"], 
+                 df[,mean(horsetail, na.rm = T), by = "season"], 
+                 df[,mean(tree, na.rm = T), by = "season"], 
+                 df[,mean(moss, na.rm = T), by = "season"], 
+                 df[,mean(fungi, na.rm = T), by = "season"],
+                 df[,mean(vascular, na.rm = T), by = "season"]),
+                 rbind(df[,sd(lichen, na.rm = T), by = "season"], 
+                 df[,sd(graminoid, na.rm = T), by = "season"], 
+                 df[,sd(forbs, na.rm = T), by = "season"], 
+                 df[,sd(shrubs, na.rm = T), by = "season"], 
+                 df[,sd(horsetail, na.rm = T), by = "season"], 
+                 df[,sd(tree, na.rm = T), by = "season"], 
+                 df[,sd(moss, na.rm = T), by = "season"], 
+                 df[,sd(fungi, na.rm = T), by = "season"],
+                 df[,sd(vascular, na.rm = T), by = "season"]),
+           plant = c(rep(c("lichen"), 5),
+                     rep(c("graminoid"), 5), 
+                     rep(c("forbs"), 5), 
+                     rep(c("shrubs"), 5), 
+                     rep(c("horsetail"), 5), 
+                     rep(c("tree"), 5), 
+                     rep(c("moss"), 5), 
+                     rep(c("fungi"), 5),
+                     rep(c("vascular"), 5)))
+
+## summary of plant type by ecotype
+data.table(rbind(df[,mean(lichen, na.rm = T), by = "Subspecies"], 
+                       df[,mean(graminoid, na.rm = T), by = "Subspecies"], 
+                       df[,mean(forbs, na.rm = T), by = "Subspecies"], 
+                       df[,mean(shrubs, na.rm = T), by = "Subspecies"], 
+                       df[,mean(horsetail, na.rm = T), by = "Subspecies"], 
+                       df[,mean(tree, na.rm = T), by = "Subspecies"], 
+                       df[,mean(moss, na.rm = T), by = "Subspecies"], 
+                       df[,mean(fungi, na.rm = T), by = "Subspecies"],
+                       df[,mean(vascular, na.rm = T), by = "Subspecies"]),
+                 rbind(df[,sd(lichen, na.rm = T), by = "Subspecies"], 
+                       df[,sd(graminoid, na.rm = T), by = "Subspecies"], 
+                       df[,sd(forbs, na.rm = T), by = "Subspecies"], 
+                       df[,sd(shrubs, na.rm = T), by = "Subspecies"], 
+                       df[,sd(horsetail, na.rm = T), by = "Subspecies"], 
+                       df[,sd(tree, na.rm = T), by = "Subspecies"], 
+                       df[,sd(moss, na.rm = T), by = "Subspecies"], 
+                       df[,sd(fungi, na.rm = T), by = "Subspecies"],
+                       df[,sd(vascular, na.rm = T), by = "Subspecies"]),
+                 plant = c(rep(c("lichen"), 6),
+                           rep(c("graminoid"), 6), 
+                           rep(c("forbs"), 6), 
+                           rep(c("shrubs"), 6), 
+                           rep(c("horsetail"), 6), 
+                           rep(c("tree"), 6), 
+                           rep(c("moss"), 6), 
+                           rep(c("fungi"), 6),
+                           rep(c("vascular"), 6)))
+
---
+++ scripts/Figure4.R
@@ -0,0 +1,67 @@
+
+## load packages
+library(data.table)
+library(ggplot2)
+
+## load data
+a1 <- readRDS("output/lichen-model.RDS")
+b1 <- readRDS("output/graminoid-model.RDS")
+c1 <- readRDS("output/vascular-model.RDS")
+
+## pull out fixed effects
+coefs <- data.table(var = rbind(data.table(rownames(data.frame(summary(a1)[6]$coefficients$cond[,1]))),
+                                data.table(rownames(data.frame(summary(b1)[6]$coefficients$cond[,1]))),
+                                data.table(rownames(data.frame(summary(c1)[6]$coefficients$cond[,1])))),
+                    coef = rbind(data.table(summary(a1)[6]$coefficients$cond[,1]), 
+                                 data.table(summary(b1)[6]$coefficients$cond[,1]), 
+                                 data.table(summary(c1)[6]$coefficients$cond[,1])),
+                    se = rbind(data.table(summary(a1)[6]$coefficients$cond[,2]), 
+                               data.table(summary(b1)[6]$coefficients$cond[,2]), 
+                               data.table(summary(c1)[6]$coefficients$cond[,2])),
+                    model = c(rep("Lichen", 13), 
+                              rep("Graminoids", 13), 
+                              rep("Vascular plants", 13)))
+setnames(coefs, c("var.V1", "coef.V1", "se.V1"), c("var", "coef", "se"))
+
+coefs
+
+png("figures/Fig4.png", width = 5000, height = 5000, units = "px", res = 600)
+ggplot(data = coefs) +
+  geom_point(aes(coef, var, color = model), size = 2, 
+             position = position_dodge(width = 0.4), 
+             alpha = 0.5) +
+  geom_errorbar(aes(coef, var, 
+                    xmin = coef - se, 
+                    xmax = coef + se,
+                    color = model), width = 0, size = 0.75,
+                position = position_dodge(width = 0.4), 
+                alpha = 0.5) +  
+  geom_vline(xintercept = 0, lty = 2) +
+  scale_color_manual(values = c("#7570b3", "#d95f02", "#1b9e77")) +
+  #labels = c("Lichen", "Graminoids", "Vascular plants")) +
+  xlab("Fixed effect coefficient estimate") + 
+  ylab("") +
+  scale_y_discrete(labels = c(`(Intercept)` = "Model Intercept",
+                              `subspecies6-Introduced-Reindeer` = "Ecotype (Introduced Reindeer)",
+                              `subspecies5-Reindeer` = "Ecotype (Reindeer)", 
+                              `subspecies4-Woodland` = "Ecotype (Woodland)",
+                              `subspecies3-Mountain` = "Ecotype (Mountain)", 
+                              `subspecies2-Barren Ground` = "Ecotype (Barren Ground)",
+                              `season5-Spring` = "Season (Spring)", 
+                              `season4-Winter` = "Season (Winter)", 
+                              `season3-Autumn` = "Season (Autumn)", 
+                              `season2-Summer` = "Season (Summer)", 
+                              `symp` = "Number of sympatric ungulates", 
+                              `latitude` = "Latitude",
+                              `datarumen` = "Data type (Rumen)")) +
+  theme(
+    legend.title = element_blank(),
+    legend.key = element_blank(),
+    legend.text = element_text(size = 10),
+    axis.title = element_text(size = 14, color = 'black'),
+    axis.text = element_text(size = 12, color = 'black'),
+    panel.grid.minor = element_blank(),
+    panel.background = element_blank(), 
+    panel.border = element_rect(colour = "black", fill=NA, size = 1)) 
+dev.off()
+
