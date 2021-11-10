429dddc5ff46ff26f77f77eb16b06afe78f0f73e
Alec L. Robitaille <robit.alec@gmail.com>
2021-11-10 14:57:06 -0800

    tidy ee output

--- scripts/extract-npp.R
+++ scripts/extract-npp.R
@@ -30,3 +30,23 @@ setnames(DT, long_names, short_names)
 
 # Convert CSV to a zipped shapefile for Earth Engine upload
 zip_path <- csv_to_zipped_shp(DT, out_path, name, coords)
+
+# Extract with EE (EE/Extract-NPP.js)
+# ...
+
+# Load output
+npp <- fread('output/lat-ee-npp.csv')
+
+# Replace names
+setnames(npp, short_names, long_names)
+
+# Remove extra columns
+npp[, `.geo` := NULL]
+npp[, `system:index` := NULL]
+
+# Why NAs?
+npp[is.na(Npp), why := 'NA in NPP image']
+
+# Merge back onto full data (with NAs in coordinates)
+DT[is.na(longitude) | is.na(latitude), why := 'NA in coordinates']
+
