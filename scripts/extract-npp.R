f62338e8dac00229a9e274ecc6550c7b22da038a
Alec L. Robitaille <robit.alec@gmail.com>
2021-11-10 12:58:34 -0800

    fix colnames, source function

--- scripts/extract-npp.R
+++ scripts/extract-npp.R
@@ -6,13 +6,20 @@
 library(data.table)
 library(wk)
 
+
+# Functions
+source('R/fwrite_wkt.R')
+
+
 # Data
 DT <- fread('input/lat.csv')
+
 
 # Variables
 out_path <- 'output'
 name <- 'lat_ee'
-coords <- c('long', 'lat')
+coords <- c('longitude', 'latitude')
+
 
 # Convert to CSV with WKT for lat/long
 fwrite_wkt(DT, out_path, name, coords)
