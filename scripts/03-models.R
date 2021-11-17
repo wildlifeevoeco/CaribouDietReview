0eb47d78602c6ffb455d678c407496894eebf335
Quinn Webber <webber.quinn@gmail.com>
2021-11-17 10:50:04 -0330

    Update 03-models.R

--- scripts/03-models.R
+++ scripts/03-models.R
@@ -6,7 +6,7 @@ library(MuMIn)
 library(ggplot2)
 
 ## load data
-sub <- fread("output/clean-data.csv")
+sub <- fread("output/clean-data-npp2.csv")
 sub$lichen <- sub$lichen/100
 sub$graminoid <- sub$graminoid/100
 sub$vascular <- sub$vascular/100
@@ -18,7 +18,6 @@ sub$symp[sub$symp == 5] <- 4
 sub$symp[sub$symp == 6] <- 4
 
 
-
 ########################### 
 ######## LICHEN ##########
 ########################### 
@@ -27,7 +26,7 @@ sub$symp[sub$symp == 6] <- 4
 a1 <- glmmTMB(lichen ~ 
                 subspecies + 
                 season + 
-                latitude + 
+                scale(Npp) + 
                 symp + 
                 data + 
                 (1|author_yr),
@@ -35,14 +34,11 @@ a1 <- glmmTMB(lichen ~
               family=beta_family,
               data = sub)
 
-summary(a1)
-performance::r2(a1)
-
 ## remove subspecies
 a2 <- glmmTMB(lichen ~ 
                 #subspecies + 
                 season + 
-                latitude + 
+                scale(Npp) + 
                 symp + 
                 data + 
                 (1|author_yr),
@@ -54,7 +50,7 @@ a2 <- glmmTMB(lichen ~
 a3 <- glmmTMB(lichen ~ 
                 subspecies + 
                 #season + 
-                latitude + 
+                scale(Npp) + 
                 symp + 
                 data + 
                 (1|author_yr),
@@ -62,11 +58,11 @@ a3 <- glmmTMB(lichen ~
               family=beta_family,
               data = sub)
 
-## remove latitude
+## remove scale(Npp)
 a4 <- glmmTMB(lichen ~ 
                 subspecies + 
                 season + 
-                #latitude + 
+                #scale(Npp) + 
                 symp + 
                 data + 
                 (1|author_yr),
@@ -78,7 +74,7 @@ a4 <- glmmTMB(lichen ~
 a5 <- glmmTMB(lichen ~ 
                 subspecies + 
                 season + 
-                latitude + 
+                scale(Npp) + 
                 #symp + 
                 data + 
                 (1|author_yr),
@@ -90,7 +86,7 @@ a5 <- glmmTMB(lichen ~
 a6 <- glmmTMB(lichen ~ 
                 #subspecies + 
                 #season + 
-                latitude + 
+                scale(Npp) + 
                 symp + 
                 data + 
                 (1|author_yr),
@@ -98,11 +94,11 @@ a6 <- glmmTMB(lichen ~
               family=beta_family,
               data = sub)
 
-## remove subspecies + latitude
+## remove subspecies + scale(Npp)
 a7 <- glmmTMB(lichen ~ 
                 #subspecies + 
                 season + 
-                #latitude + 
+                #scale(Npp) + 
                 symp + 
                 data + 
                 (1|author_yr),
@@ -115,7 +111,7 @@ a7 <- glmmTMB(lichen ~
 a8 <- glmmTMB(lichen ~ 
                 #subspecies + 
                 season + 
-                latitude + 
+                scale(Npp) + 
                 #symp + 
                 data + 
                 (1|author_yr),
@@ -123,11 +119,11 @@ a8 <- glmmTMB(lichen ~
               family=beta_family,
               data = sub)
 
-## remove season + latitude
+## remove season + scale(Npp)
 a9 <- glmmTMB(lichen ~ 
                 subspecies + 
                 #season + 
-                #latitude + 
+                #scale(Npp) + 
                 symp + 
                 data + 
                 (1|author_yr),
@@ -135,11 +131,11 @@ a9 <- glmmTMB(lichen ~
               family=beta_family,
               data = sub)
 
-## remove season + latitude
+## remove season + scale(Npp)
 a10 <- glmmTMB(lichen ~ 
                 subspecies + 
                 #season + 
-                latitude + 
+                scale(Npp) + 
                 #symp + 
                 data + 
                 (1|author_yr),
@@ -148,11 +144,11 @@ a10 <- glmmTMB(lichen ~
               data = sub)
 
 
-## remove latitude + sympatry
+## remove scale(Npp) + sympatry
 a11 <- glmmTMB(lichen ~ 
                  subspecies + 
                  season + 
-                 #latitude + 
+                 #scale(Npp) + 
                  #symp + 
                  data + 
                  (1|author_yr),
@@ -164,7 +160,7 @@ a11 <- glmmTMB(lichen ~
 a12 <- glmmTMB(lichen ~ 
                  subspecies + 
                  #season + 
-                 #latitude + 
+                 #scale(Npp) + 
                  #symp + 
                  data + 
                  (1|author_yr),
@@ -176,7 +172,7 @@ a12 <- glmmTMB(lichen ~
 a13 <- glmmTMB(lichen ~ 
                  #subspecies + 
                  season + 
-                 #latitude + 
+                 #scale(Npp) + 
                  #symp + 
                  data + 
                  (1|author_yr),
@@ -184,11 +180,11 @@ a13 <- glmmTMB(lichen ~
                family=beta_family,
                data = sub)
 
-## only latitude
+## only scale(Npp)
 a14 <- glmmTMB(lichen ~ 
                  #subspecies + 
                  #season + 
-                 latitude + 
+                 scale(Npp) + 
                  #symp + 
                  data + 
                  (1|author_yr),
@@ -196,11 +192,11 @@ a14 <- glmmTMB(lichen ~
                family=beta_family,
                data = sub)
 
-## only latitude
+## only scale(Npp)
 a15 <- glmmTMB(lichen ~ 
                  #subspecies + 
                  #season + 
-                 #latitude + 
+                 #scale(Npp) + 
                  symp + 
                  data + 
                  (1|author_yr),
@@ -216,8 +212,12 @@ lichen_mods$weights <- round(Weights(AICc(a1, a2, a3, a4, a5, a6, a7, a8,
                                     a9, a10, a11, a12, a13, a14, a15)),3)
 
 
+## top model:
+summary(a11)
+performance::r2(a11)
 
-saveRDS(a5, "output/lichen-model.RDS")
+
+saveRDS(a11, "output/lichen-model.RDS")
 
 
 ############################# 
@@ -228,7 +228,7 @@ saveRDS(a5, "output/lichen-model.RDS")
 b1 <- glmmTMB(graminoid ~ 
                 subspecies + 
                 season + 
-                latitude + 
+                scale(Npp) + 
                 symp + 
                 data + 
                 (1|author_yr),
@@ -236,14 +236,11 @@ b1 <- glmmTMB(graminoid ~
               family=beta_family,
               data = sub)
 
-summary(b1)
-performance::r2(b1)
-
 ## remove subspecies
 b2 <- glmmTMB(graminoid ~ 
                 #subspecies + 
                 season + 
-                latitude + 
+                scale(Npp) + 
                 symp + 
                 data + 
                 (1|author_yr),
@@ -255,7 +252,7 @@ b2 <- glmmTMB(graminoid ~
 b3 <- glmmTMB(graminoid ~ 
                 subspecies + 
                 #season + 
-                latitude + 
+                scale(Npp) + 
                 symp + 
                 data + 
                 (1|author_yr),
@@ -263,11 +260,11 @@ b3 <- glmmTMB(graminoid ~
               family=beta_family,
               data = sub)
 
-## remove latitude
+## remove scale(Npp)
 b4 <- glmmTMB(graminoid ~ 
                 subspecies + 
                 season + 
-                #latitude + 
+                #scale(Npp) + 
                 symp + 
                 data + 
                 (1|author_yr),
@@ -279,7 +276,7 @@ b4 <- glmmTMB(graminoid ~
 b5 <- glmmTMB(graminoid ~ 
                 subspecies + 
                 season + 
-                latitude + 
+                scale(Npp) + 
                 #symp + 
                 data + 
                 (1|author_yr),
@@ -291,7 +288,7 @@ b5 <- glmmTMB(graminoid ~
 b6 <- glmmTMB(graminoid ~ 
                 #subspecies + 
                 #season + 
-                latitude + 
+                scale(Npp) + 
                 symp + 
                 data + 
                 (1|author_yr),
@@ -299,11 +296,11 @@ b6 <- glmmTMB(graminoid ~
               family=beta_family,
               data = sub)
 
-## remove subspecies + latitude
+## remove subspecies + scale(Npp)
 b7 <- glmmTMB(graminoid ~ 
                 #subspecies + 
                 season + 
-                #latitude + 
+                #scale(Npp) + 
                 symp + 
                 data + 
                 (1|author_yr),
@@ -316,7 +313,7 @@ b7 <- glmmTMB(graminoid ~
 b8 <- glmmTMB(graminoid ~ 
                 #subspecies + 
                 season + 
-                latitude + 
+                scale(Npp) + 
                 #symp + 
                 data + 
                 (1|author_yr),
@@ -324,11 +321,11 @@ b8 <- glmmTMB(graminoid ~
               family=beta_family,
               data = sub)
 
-## remove season + latitude
+## remove season + scale(Npp)
 b9 <- glmmTMB(graminoid ~ 
                 subspecies + 
                 #season + 
-                #latitude + 
+                #scale(Npp) + 
                 symp + 
                 data + 
                 (1|author_yr),
@@ -336,11 +333,11 @@ b9 <- glmmTMB(graminoid ~
               family=beta_family,
               data = sub)
 
-## remove season + latitude
+## remove season + scale(Npp)
 b10 <- glmmTMB(graminoid ~ 
                  subspecies + 
                  #season + 
-                 latitude + 
+                 scale(Npp) + 
                  #symp + 
                  data + 
                  (1|author_yr),
@@ -349,11 +346,11 @@ b10 <- glmmTMB(graminoid ~
                data = sub)
 
 
-## remove latitude + sympatry
+## remove scale(Npp) + sympatry
 b11 <- glmmTMB(graminoid ~ 
                  subspecies + 
                  season + 
-                 #latitude + 
+                 #scale(Npp) + 
                  #symp + 
                  data + 
                  (1|author_yr),
@@ -365,7 +362,7 @@ b11 <- glmmTMB(graminoid ~
 b12 <- glmmTMB(graminoid ~ 
                  subspecies + 
                  #season + 
-                 #latitude + 
+                 #scale(Npp) + 
                  #symp + 
                  data + 
                  (1|author_yr),
@@ -377,7 +374,7 @@ b12 <- glmmTMB(graminoid ~
 b13 <- glmmTMB(graminoid ~ 
                  #subspecies + 
                  season + 
-                 #latitude + 
+                 #scale(Npp) + 
                  #symp + 
                  data + 
                  (1|author_yr),
@@ -385,11 +382,11 @@ b13 <- glmmTMB(graminoid ~
                family=beta_family,
                data = sub)
 
-## only latitude
+## only scale(Npp)
 b14 <- glmmTMB(graminoid ~ 
                  #subspecies + 
                  #season + 
-                 latitude + 
+                 scale(Npp) + 
                  #symp + 
                  data + 
                  (1|author_yr),
@@ -397,11 +394,11 @@ b14 <- glmmTMB(graminoid ~
                family=beta_family,
                data = sub)
 
-## only latitude
+## only scale(Npp)
 b15 <- glmmTMB(graminoid ~ 
                  #subspecies + 
                  #season + 
-                 #latitude + 
+                 #scale(Npp) + 
                  symp + 
                  data + 
                  (1|author_yr),
@@ -416,8 +413,12 @@ graminoid_mods$deltaAIC <- graminoid_mods$AICc - min(graminoid_mods$AICc)
 graminoid_mods$weights <- round(Weights(AICc(b1, b2, b3, b4, b5, b6, b7, b8,
                                              b9, b10, b11, b12, b13, b14, b15)),3)
 
+## top model
+summary(b15)
+performance::r2(b15)
 
-saveRDS(b6, "output/graminoid-model.RDS")
+
+saveRDS(b15, "output/graminoid-model.RDS")
 
 
 ##################################### 
@@ -427,7 +428,7 @@ saveRDS(b6, "output/graminoid-model.RDS")
 c1 <- glmmTMB(vascular ~ 
                 subspecies + 
                 season + 
-                latitude + 
+                scale(Npp) + 
                 symp + 
                 data + 
                 (1|author_yr),
@@ -442,7 +443,7 @@ performance::r2(c1)
 c2 <- glmmTMB(vascular ~ 
                 #subspecies + 
                 season + 
-                latitude + 
+                scale(Npp) + 
                 symp + 
                 data + 
                 (1|author_yr),
@@ -454,7 +455,7 @@ c2 <- glmmTMB(vascular ~
 c3 <- glmmTMB(vascular ~ 
                 subspecies + 
                 #season + 
-                latitude + 
+                scale(Npp) + 
                 symp + 
                 data + 
                 (1|author_yr),
@@ -462,11 +463,11 @@ c3 <- glmmTMB(vascular ~
               family=beta_family,
               data = sub)
 
-## remove latitude
+## remove scale(Npp)
 c4 <- glmmTMB(vascular ~ 
                 subspecies + 
                 season + 
-                #latitude + 
+                #scale(Npp) + 
                 symp + 
                 data + 
                 (1|author_yr),
@@ -478,7 +479,7 @@ c4 <- glmmTMB(vascular ~
 c5 <- glmmTMB(vascular ~ 
                 subspecies + 
                 season + 
-                latitude + 
+                scale(Npp) + 
                 #symp + 
                 data + 
                 (1|author_yr),
@@ -490,7 +491,7 @@ c5 <- glmmTMB(vascular ~
 c6 <- glmmTMB(vascular ~ 
                 #subspecies + 
                 #season + 
-                latitude + 
+                scale(Npp) + 
                 symp + 
                 data + 
                 (1|author_yr),
@@ -498,11 +499,11 @@ c6 <- glmmTMB(vascular ~
               family=beta_family,
               data = sub)
 
-## remove subspecies + latitude
+## remove subspecies + scale(Npp)
 c7 <- glmmTMB(vascular ~ 
                 #subspecies + 
                 season + 
-                #latitude + 
+                #scale(Npp) + 
                 symp + 
                 data + 
                 (1|author_yr),
@@ -515,7 +516,7 @@ c7 <- glmmTMB(vascular ~
 c8 <- glmmTMB(vascular ~ 
                 #subspecies + 
                 season + 
-                latitude + 
+                scale(Npp) + 
                 #symp + 
                 data + 
                 (1|author_yr),
@@ -523,11 +524,11 @@ c8 <- glmmTMB(vascular ~
               family=beta_family,
               data = sub)
 
-## remove season + latitude
+## remove season + scale(Npp)
 c9 <- glmmTMB(vascular ~ 
                 subspecies + 
                 #season + 
-                #latitude + 
+                #scale(Npp) + 
                 symp + 
                 data + 
                 (1|author_yr),
@@ -535,11 +536,11 @@ c9 <- glmmTMB(vascular ~
               family=beta_family,
               data = sub)
 
-## remove season + latitude
+## remove season + scale(Npp)
 c10 <- glmmTMB(vascular ~ 
                  subspecies + 
                  #season + 
-                 latitude + 
+                 scale(Npp) + 
                  #symp + 
                  data + 
                  (1|author_yr),
@@ -548,11 +549,11 @@ c10 <- glmmTMB(vascular ~
                data = sub)
 
 
-## remove latitude + sympatry
+## remove scale(Npp) + sympatry
 c11 <- glmmTMB(vascular ~ 
                  subspecies + 
                  season + 
-                 #latitude + 
+                 #scale(Npp) + 
                  #symp + 
                  data + 
                  (1|author_yr),
@@ -564,7 +565,7 @@ c11 <- glmmTMB(vascular ~
 c12 <- glmmTMB(vascular ~ 
                  subspecies + 
                  #season + 
-                 #latitude + 
+                 #scale(Npp) + 
                  #symp + 
                  data + 
                  (1|author_yr),
@@ -576,7 +577,7 @@ c12 <- glmmTMB(vascular ~
 c13 <- glmmTMB(vascular ~ 
                  #subspecies + 
                  season + 
-                 #latitude + 
+                 #scale(Npp) + 
                  #symp + 
                  data + 
                  (1|author_yr),
@@ -584,11 +585,11 @@ c13 <- glmmTMB(vascular ~
                family=beta_family,
                data = sub)
 
-## only latitude
+## only scale(Npp)
 c14 <- glmmTMB(vascular ~ 
                  #subspecies + 
                  #season + 
-                 latitude + 
+                 scale(Npp) + 
                  #symp + 
                  data + 
                  (1|author_yr),
@@ -596,11 +597,11 @@ c14 <- glmmTMB(vascular ~
                family=beta_family,
                data = sub)
 
-## only latitude
+## only scale(Npp)
 c15 <- glmmTMB(vascular ~ 
                  #subspecies + 
                  #season + 
-                 #latitude + 
+                 #scale(Npp) + 
                  symp + 
                  data + 
                  (1|author_yr),
@@ -615,6 +616,8 @@ vascular_mods$deltaAIC <- vascular_mods$AICc - min(vascular_mods$AICc)
 vascular_mods$weights <- round(Weights(AICc(c1, c2, c3, c4, c5, c6, c7, c8,
                                              c9, c10, c11, c12, c13, c14, c15)),3)
 
+summary(c8)
+performance::r2(c8)
 
-saveRDS(c1, "output/vascular-model.RDS")
+saveRDS(c8, "output/vascular-model.RDS")
 
