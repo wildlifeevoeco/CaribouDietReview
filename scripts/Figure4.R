d2abd5c7f7e10543a88318dd0002a838573553bd
Quinn Webber <webber.quinn@gmail.com>
2021-11-26 10:47:10 -0330

    Update Figure4.R

--- scripts/Figure4.R
+++ scripts/Figure4.R
@@ -3,9 +3,16 @@
 
 library(data.table)
 library(ggplot2)
+library(visreg)
 library(gridExtra)
 
 sub <- fread("output/clean-data-all.csv")
+
+## multiply NPP * 0.0001
+sub$Npp <- sub$Npp*0.0001
+
+gram <- readRDS("output/graminoid-model.RDS")
+lichen <- readRDS("output/lichen-model.RDS")
 
 gram <- sub[,c("Npp", "graminoid")]
 gram$plant <- "Graminoid"
@@ -21,14 +28,15 @@ col = c("black", "darkgrey")
 png("figures/fig4.png", width = 4000, height = 4000, units = "px", res = 600)
 ggplot(df, aes(Npp, diet, color = plant)) +
   geom_jitter(aes(shape = plant), 
-              width = 100, 
+              width = 0.01, 
               alpha = 0.75,
               size = 2) +
   geom_smooth(aes(color = plant), 
               method = "lm", se = F) +
   scale_color_manual(values = col) +
-  xlab("Average annual net primary productivity") +
   ylab("Percentage of plant type in diet") + 
+  labs(x = expression(Average~annual~net~primary~productivity~~(frac(kg~C, m^{2})))) +
+  ylab("Percentage of plant type in diet") +
   ylim(0,100) +
   theme(legend.position = c(0.8,0.9), 
         legend.key = element_blank(),
